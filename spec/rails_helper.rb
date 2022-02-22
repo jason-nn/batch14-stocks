9e044ec20b2190f85ed65c99413b24a7cb7fdbee
Jason Ho <jason.ho@obf.ateneo.edu>
2022-02-22 20:08:45 +0800

    Comment out devise in rails helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,7 +3,9 @@ require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 # Prevent database truncation if the environment is production
-abort("The Rails environment is running in production mode!") if Rails.env.production?
+if Rails.env.production?
+  abort('The Rails environment is running in production mode!')
+end
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
@@ -59,9 +61,10 @@ RSpec.configure do |config|
 
   # Filter lines from Rails gems in backtraces.
   config.filter_rails_from_backtrace!
+
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 
   config.include FactoryBot::Syntax::Methods
-  config.include Devise::Test::IntegrationHelpers
+  # config.include Devise::Test::IntegrationHelpers
 end
