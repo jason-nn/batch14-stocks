e062ec55c4eac9b46dd5c8b26467093ba195ef23
Jason Ho <jason.ho@obf.ateneo.edu>
2022-02-22 23:24:57 +0800

    Changed controllers to a single pages controller

--- app/controllers/transactions_controller.rb
+++
@@ -1,7 +0,0 @@
-class TransactionsController < ApplicationController
-  before_action :authenticate_user!
-
-  def index
-    Transaction.all
-  end
-end
--- spec/system/transactions_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe 'Transactions', type: :system do
-  before { driven_by(:rack_test) }
-end
