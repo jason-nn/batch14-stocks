39ce2bca54d90299dbe1855c35bd5d98f37c6ab7
Jason Ho <jason.ho@obf.ateneo.edu>
2022-02-23 20:56:14 +0800

    Add inclusion in transaction model

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,6 +1,10 @@
 class Transaction < ApplicationRecord
   belongs_to :user
 
-  validates :action, presence: true
+  validates :action,
+            presence: true,
+            inclusion: {
+              in: ['cash in', 'purchase', 'sale'],
+            }
   validates :amount, presence: true
 end
