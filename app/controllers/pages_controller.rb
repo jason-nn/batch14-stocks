3eadbc06330770c82699d443c916aeb74fef98c0
Jason Ho <jason.ho@obf.ateneo.edu>
2022-02-23 23:59:08 +0800

    Add routes, controller methods, and get request specs

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,7 +9,7 @@ class PagesController < ApplicationController
     #
   end
 
-  def market
+  def account
     #
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,18 @@ Rails.application.routes.draw do
 
   root 'pages#home'
   get '/portfolio', to: 'pages#portfolio', as: 'portfolio'
+  get '/account', to: 'pages#account', as: 'account'
+
+  get '/transactions', to: 'transactions#index', as: 'transactions'
+  post '/cashin', to: 'transactions#cashin', as: 'cashin'
+  post '/buy', to: 'transactions#buy', as: 'buy'
+  post '/sell', to: 'transactions#sell', as: 'sell'
+
+  get '/users', to: 'users#index', as: 'users'
+  get '/users/:id', to: 'users#show', as: 'user'
+  get '/users/new', to: 'users#new', as: 'new_user'
+  get '/users/:id/edit', to: 'users#edit', as: 'edit_user'
+  post '/users/create', to: 'users#create', as: 'create_user'
+  patch '/users/:id/update', to: 'users#update', as: 'update_user'
+  patch '/users/:id/approve', to: 'users#approve', as: 'approve_user'
 end
