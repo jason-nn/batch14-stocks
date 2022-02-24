ce8ddfde0c37f56de1a4b112ad0ceed1707ec9db
MJ Aguirre <michaelaquila1998@gmail.com>
2022-02-24 19:47:39 +0800

    Add stock spec

--- spec/models/stock_spec.rb
+++ spec/models/stock_spec.rb
@@ -1,4 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe Stock, type: :model do
+  context 'Validations' do
+    it 'should be saved when all information is provided ' do
+      stock = Stock.new(symbol: 'GOOG', price: 2500, company: 'Alphabet Inc.')
+      expect(stock.save).to eq true
+    end
+
+    it 'should not be saved when symbol is an empty string' do
+      stock = Stock.new(symbol: '', price: 2500, company: 'Alphabet Inc.')
+      expect(stock.save).to eq false
+    end
+
+    it 'should not be saved when price is an empty string' do
+      stock = Stock.new(symbol: 'GOOG', price: '', company: 'Alphabet Inc.')
+      expect(stock.save).to eq false
+    end
+
+    it 'should not be saved when company is an empty string' do
+      stock = Stock.new(symbol: 'GOOG', price: 2500, company: '')
+      expect(stock.save).to eq false
+    end
+
+    it 'should not be saved when symbol is nil' do
+      stock = Stock.new(symbol: nil, price: 2500, company: 'Alphabet Inc.')
+      expect(stock.save).to eq false
+    end
+
+    it 'should not be saved when price is nil' do
+      stock = Stock.new(symbol: 'GOOG', price: nil, company: 'Alphabet Inc.')
+      expect(stock.save).to eq false
+    end
+
+    it 'should not be saved when company is nil' do
+      stock = Stock.new(symbol: 'GOOG', price: 2500, company: nil)
+      expect(stock.save).to eq false
+    end
+  end
 end
