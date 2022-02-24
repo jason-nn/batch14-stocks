6277d2e43c2bf7cfa0d80eb4a3ad94871c43af14
Jason Ho <jason.ho@obf.ateneo.edu>
2022-02-24 11:59:05 +0800

    Add pages, edit request spec

--- spec/requests/transactions_spec.rb
+++ spec/requests/transactions_spec.rb
@@ -6,9 +6,9 @@ RSpec.describe 'Transactions Request Spec', type: :request do
     sign_in @user
   end
 
-  it 'cashin' do
-    get cashin_path
+  it 'transactions' do
+    get transactions_path
     expect(response).to have_http_status(200)
-    expect(response).to render_template(:cashin)
+    expect(response).to render_template(:index)
   end
 end
