db052145a3429754904fdd24f3df92aae054631f
MJ Aguirre <michaelaquila1998@gmail.com>
2022-02-26 18:18:02 +0800

    Add users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,30 +2,41 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    #
+    @user = User.all
   end
 
   def show
-    #
+    @user = User.find(params[:id])
   end
 
   def new
-    #
+    @user = User.new
   end
 
   def create
-    #
+    @user = User.new(user_params)
   end
 
   def edit
-    #
+    @user = User.find(params[:id])
   end
 
   def update
-    #
+    if @user.update(user_params)
+      redirect_to users_path, notice: @user.username + ' has been updated!'
+    else
+      render :edit
+    end
   end
 
   def approve
-    #
+    @user = User.find(params[:id])
+    @user.update(approved: true)
+    redirect_to users_path, notice: @user.username + ' has been approved!'
+  end
+
+  private 
+  def user_params
+    params.require(:user).permit(:name, :surname, :username, :email, :password)
   end
 end
