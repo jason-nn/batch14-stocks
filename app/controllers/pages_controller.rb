2b1e81fc0555f7b81deeef1e5bb6b16b9a11aa3f
Jason Ho <jason.ho@obf.ateneo.edu>
2022-02-26 12:51:49 +0800

    Move out portfolio and balance to private methods

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,28 @@
 class PagesController < ApplicationController
   before_action :authenticate_user!, except: %i[home]
+  before_action :set_user
+  before_action :set_portfolio, only: %i[portfolio]
+  before_action :set_balance, only: %i[account]
 
   def home
     #
   end
 
   def portfolio
+    #
+  end
+
+  def account
+    #
+  end
+
+  private
+
+  def set_user
+    @user = current_user
+  end
+
+  def set_portfolio
     portfolio = {}
 
     transactions = Transaction.where(user_id: current_user.id)
@@ -20,7 +37,7 @@ class PagesController < ApplicationController
     @portfolio = portfolio
   end
 
-  def account
+  def set_balance
     @balance = Transaction.where(user_id: current_user.id).pluck(:amount).sum
   end
 end
