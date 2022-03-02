2ab6e90378cd334635604f2a54c023b5ebc5cb7f
Jason Ho <jason.ho@obf.ateneo.edu>
2022-03-02 20:10:25 +0800

    Encrypted keys

--- services/iex_service_object.rb
+++ services/iex_service_object.rb
@@ -2,20 +2,11 @@ class IEXServiceObject
   def client
     return(
       IEX::Api::Client.new(
-        publishable_token: iex_publishable_token,
-        secret_token: iex_secret_token,
+        publishable_token:
+          Rails.application.credentials.iex[:publishable_token],
+        secret_token: Rails.application.credentials.iex[:secret_token],
         endpoint: 'https://cloud.iexapis.com/v1',
       )
     )
-  end
-
-  private
-
-  def iex_publishable_token
-    return 'pk_714d506665774bd4911a522edde00754'
-  end
-
-  def iex_secret_token
-    return 'sk_5a1c52d71b9a4d1d880b1f44646e44af'
   end
 end
