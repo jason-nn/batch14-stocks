ac6fb74df537b5ae811f2c396b6cb3f0ce94b1eb
Jason Ho <jason.ho@obf.ateneo.edu>
2022-03-02 20:00:58 +0800

    Add IEX service object

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+require './services/iex_service_object.rb'
+
+client = IEXServiceObject.new.client
+
 admin = User.create(email: 'jason@admin.com', password: 'password', admin: true)
 
 jason =
@@ -13,13 +17,6 @@ jason =
     email: 'jason.ho@obf.ateneo.edu',
     password: 'password',
     admin: false,
-  )
-
-client =
-  IEX::Api::Client.new(
-    publishable_token: 'pk_714d506665774bd4911a522edde00754',
-    secret_token: 'sk_5a1c52d71b9a4d1d880b1f44646e44af',
-    endpoint: 'https://cloud.iexapis.com/v1',
   )
 
 stocks = client.stock_market_list(:mostactive)
